80fce84bd510a3c0f29e5dcfe72aac87f0db0dfd
R-second <ritsu920@icloud.com>
2025-05-16 15:16:14 +0900

    とりあえずleapmotionのデータを取得

--- src/main.py
+++ src/main.py
@@ -10,8 +10,13 @@ def main():
 
     while not game_scene.should_quit():
         leap_data = leap_input.get_hand_position()
-        hand_pos = random.random()  # テスト用
-        game_scene.update(hand_pos)
+        if leap_data is None:
+            continue
+        param = leap_data.finger_directions_to_parameter()
+        if param is None:
+            continue
+        # hand_pos = random.random()  # テスト用
+        game_scene.update(param)
 
     game_scene.cleanup()
 
